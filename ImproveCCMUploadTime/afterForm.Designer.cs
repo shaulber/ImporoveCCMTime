55d84536a6ce7f9776c0baa86ff4ded51b3f2ff3
sberkovi <shaul_berkovich@bmc.com>
2018-06-15 16:12:40 +0300

    push

--- ImproveCCMUploadTime/afterForm.Designer.cs
+++ ImproveCCMUploadTime/afterForm.Designer.cs
@@ -30,7 +30,11 @@
         {
             this.treeList1 = new DevExpress.XtraTreeList.TreeList();
             this.colName = new DevExpress.XtraTreeList.Columns.TreeListColumn();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             ((System.ComponentModel.ISupportInitialize)(this.treeList1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // treeList1
@@ -38,13 +42,14 @@
             this.treeList1.Columns.AddRange(new DevExpress.XtraTreeList.Columns.TreeListColumn[] {
             this.colName});
             this.treeList1.DataSource = null;
-            this.treeList1.Location = new System.Drawing.Point(13, 13);
+            this.treeList1.Location = new System.Drawing.Point(17, 16);
+            this.treeList1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.treeList1.Name = "treeList1";
             this.treeList1.OptionsBehavior.Editable = false;
             this.treeList1.OptionsView.ShowColumns = false;
             this.treeList1.OptionsView.ShowHorzLines = false;
             this.treeList1.OptionsView.ShowIndicator = false;
-            this.treeList1.Size = new System.Drawing.Size(249, 317);
+            this.treeList1.Size = new System.Drawing.Size(228, 579);
             this.treeList1.TabIndex = 0;
             // 
             // colName
@@ -55,16 +60,35 @@
             this.colName.Visible = true;
             this.colName.VisibleIndex = 0;
             // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(265, 16);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(652, 579);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
             // AfterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 368);
+            this.ClientSize = new System.Drawing.Size(929, 620);
+            this.Controls.Add(this.gridControl1);
             this.Controls.Add(this.treeList1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AfterForm";
             this.Text = "afterForm";
             this.Load += new System.EventHandler(this.AfterForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.treeList1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -73,5 +97,7 @@
 
         private DevExpress.XtraTreeList.TreeList treeList1;
         private DevExpress.XtraTreeList.Columns.TreeListColumn colName;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
     }
 }
