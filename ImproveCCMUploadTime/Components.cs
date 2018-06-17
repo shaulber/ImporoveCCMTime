91a6447b38aba46fe3ad150645941ba57ce86309
sberkovi <shaul_berkovich@bmc.com>
2018-06-17 08:32:47 +0300

    Map deserialize

--- ImproveCCMUploadTime/Components.cs
+++ ImproveCCMUploadTime/Components.cs
@@ -1,21 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Xml.Serialization;
 
 
 namespace ImproveCCMUploadTime
 {
-    [XmlRoot("Components")]
+    [Serializable]
     public class Components 
     {
-        [XmlElement("Compnent")]
-        public List<Compnent> Steps { get; set; }
+        [XmlArray("Component")]
+        [XmlArrayItem("Component")]
+        [XmlElement("Component")]
+        public List<Component> Component { get; set; }
     }
 
-    public class Compnent
+    [Serializable]
+    public class Component
     {
-        [XmlElement("type")]
+        [XmlElement("ComponentKey")]
+        public ComponentKey ComponentKey { get; set; }
+
+        [XmlElement("Attributes")]
+        public Attributes Attributes { get; set; }
+
+        [XmlArray("Sub_components"), XmlArrayItem("Component")]
+        public List<Component> Sub_components { get; set; }
+        
+
+    }
+
+    [Serializable]
+    public class Sub_components
+    {
+
+    }
+
+    [Serializable]
+    public class Attributes
+    {
+    }
+
+    [Serializable]
+    public class ComponentKey
+    { 
         public string Type { get; set; }
-      
+        public string Name { get; set; }
+        public string Host { get; set; }
     }
 }
--- ImproveCCMUploadTime/Topology.cs
+++ ImproveCCMUploadTime/Topology.cs
@@ -1,10 +1,17 @@
-﻿using System.Xml.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace ImproveCCMUploadTime
 {
+    [Serializable]
     [XmlRoot("Get_components_response")]
     public class Topology
     {
-        public string Components { get; set; }
+
+        //  [XmlElement("Components")]
+        // public Components Components { get; set; }
+        [XmlArray("Components"), XmlArrayItem("Component")]
+        public List<Component> Components { get; set; }
     }
 }
